d9896c9307fa84b39236c4b0d9eedfd081947161
Dmitry 'demqa' Stepanov <greatsteamguard@gmail.com>
2022-03-30 13:32:49 +0300

    Now it has FPS counter

--- Src/mandelbrot.hpp
+++ Src/mandelbrot.hpp
@@ -5,6 +5,7 @@
 #include <immintrin.h>
 #include <cmath>
 #include <cstdlib>
+#include <string.h>
 
 namespace Mandelbrot
 {
@@ -14,16 +15,20 @@ namespace Mandelbrot
         PixelsAreNullptr     = 1,
         FailedLoadingTexture = 2,
     };
-
     struct Config
     {
         sf::RenderWindow &window;
+        sf::Clock        & clock;
+        sf::Font         &  font;
 
         unsigned int     *pixels;
+        char             *string;
+
 
         // current settings of Mandelbrot
 
         // unsigned int colorscheme(unsigned int number);
+        float             FPS = 0;
 
         double xCurrentCenter = 0;
         double yCurrentCenter = 0;
